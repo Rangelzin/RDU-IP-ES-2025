fec274b9e5e6539ec7f52629e00b18b43a3ed1b8
Tiago Sales Ribeiro <tiago2007sales@gmail.com>
2025-06-12 20:41:58 -0300

    refactor: atualização da struct exames.go ♻

--- backend/models/exames.go
+++ backend/models/exames.go
@@ -4,9 +4,10 @@ import (
 	"time"
 )
 
-type Pacientes struct {
+type Exames struct {
 	Id             int       `json:"id"`
 	Paciente_id    int       `json:"paciente_id"`
+	Paciente_name  string    `json:"paciente_name"`
 	Protocolo      string    `json:"protocolo"`
 	Prontuario     string    `json:"prontuario"`
 	Data_resultado time.Time `json:"data_resultado"`
