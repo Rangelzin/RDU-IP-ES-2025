a2239d8d20cc499a4f2144c66e8696e7e09dbfa2
Rangelzin <victorrangeltasse85@hotmai.com>
2025-06-23 14:54:18 -0300

    feat: adiciona ao grupo de rota o main-paciente e leves ajustes  ✨

--- backend/routes/webPagesRoutes.go
+++ backend/routes/webPagesRoutes.go
@@ -4,19 +4,21 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-
 func RegisterAdminPages(rg *gin.RouterGroup) {
-	rg.GET("/", func(c *gin.Context) {c.File("../frontend/public/pages/main/main_admin.html")})
-	rg.GET("/paciente", func(c *gin.Context) {c.File("../frontend/public/pages/main/admin/admin_paciente.html")})
-	rg.GET("/dashboard", func(c *gin.Context) {c.File("../frontend/public/pages/main/admin/dashboard_admin.html")})
-	rg.GET("/usuario", func(c *gin.Context) {c.File("../frontend/public/pages/main/admin/admin_usuario.html")})
-	rg.GET("/usuario/criar", func(c *gin.Context) {c.File("../frontend/public/pages/main/admin/criar_usuario.html")})
-	rg.GET("/usuario/editar", func(c *gin.Context) {c.File("../frontend/public/pages/main/admin/editar_usuario.html")})
+	rg.GET("/", func(c *gin.Context) { c.File("../frontend/public/pages/main/main_admin.html") })
+	rg.GET("/paciente", func(c *gin.Context) { c.File("../frontend/public/pages/main/admin/admin_paciente.html") })
+	rg.GET("/dashboard", func(c *gin.Context) { c.File("../frontend/public/pages/main/admin/dashboard_admin.html") })
+	rg.GET("/usuario", func(c *gin.Context) { c.File("../frontend/public/pages/main/admin/admin_usuario.html") })
+	rg.GET("/usuario/criar", func(c *gin.Context) { c.File("../frontend/public/pages/main/admin/criar_usuario.html") })
+	rg.GET("/usuario/editar", func(c *gin.Context) { c.File("../frontend/public/pages/main/admin/editar_usuario.html") })
 }
 
 func RegisterUserPages(rg *gin.RouterGroup) {
-	rg.GET("/", func(c *gin.Context) {c.File("../frontend/public/pages/main/main_geral.html")})
-	rg.GET("/usuario/exame", func(c *gin.Context) {c.File("../frontend/public/pages/main/users/exam_page_starterInfo.html")})
+	rg.GET("/", func(c *gin.Context) { c.File("../frontend/public/pages/main/main_geral.html") })
+	rg.GET("/usuario/exame", func(c *gin.Context) { c.File("../frontend/public/pages/main/users/exam_page_starterInfo.html") })
+	rg.GET("/ACS", func(c *gin.Context) { c.File("../frontend/public/pages/main/main_agenteComunitario.html") })
 }
 
-
+func RegisterPatientPages(rg *gin.RouterGroup) {
+	rg.GET("/", func(c *gin.Context) { c.File("../frontend/public/pages/main/main_paciente.html") })
+}
--- backend/server/router.go
+++ backend/server/router.go
@@ -6,14 +6,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func SetupRouter (deps *Dependencies) *gin.Engine {
+func SetupRouter(deps *Dependencies) *gin.Engine {
 	r := gin.Default()
 
 	r.Static("/assets", "../frontend/public/assets")
 	r.Static("/app", "../frontend/app")
 	r.Static("/environment", "../frontend/environment")
 	r.Static("/src", "../frontend/src")
-	
+
 	r.Use(middleware.CORSMiddleware())
 	r.Use(middleware.TimingMiddleware())
 	// authMiddleware := deps.Middleware.AuthenticatorMiddleware()
@@ -31,16 +31,14 @@ func SetupRouter (deps *Dependencies) *gin.Engine {
 	routes.RegisterUserPages(rg)
 
 	rg = r.Group("/paciente")
+	routes.RegisterPatientPages(rg)
 
 	rg = r.Group("/api")
-	// rg.Use(authMiddleware)	
+	// rg.Use(authMiddleware)
 	routes.RegisterAPIPacienteRoutes(rg, deps.PacienteHandler)
 	routes.RegisterAPIUserRoutes(rg, deps.UserHandler)
 	routes.RegisterAPIExamRoutes(rg, deps.ExamHandler)
-	
 
-	
-	r.NoRoute(func(c *gin.Context) {c.File("../frontend/public/pages/404.html")})
-
+	r.NoRoute(func(c *gin.Context) { c.File("../frontend/public/pages/404.html") })
 	return r
 }
